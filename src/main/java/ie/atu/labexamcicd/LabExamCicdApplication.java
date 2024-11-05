18ca4ef8a8b8508c027eea5db43e14a71a2bbf63
G00425238@atu.ie <g00425238@atu,ie>
2024-11-05 10:05:55 +0000

    Created Project

---
+++ src/main/java/ie/atu/labexamcicd/LabExamCicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/labexamcicd/LabExamCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
